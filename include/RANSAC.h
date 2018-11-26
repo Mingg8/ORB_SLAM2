be3bba5911083f91c08b7881b3d20847d3b551a0
mjlee <mj.lee@bearrobotics.ai>
2018-11-26 18:19:14 +0900

    RANSAC.cc data loading developed

--- include/RANSAC.h
+++ include/RANSAC.h
@@ -8,20 +8,24 @@ namespace ORB_SLAM2
 class RANSAC
 {
 public:
-    RANSAC(std::vector<std::vector<float>>, std::vector<std::vector<float>>);
+    RANSAC(std::vector<std::vector<float>>&, std::vector<std::vector<float>>&);
     Eigen::Matrix4f* getMtx();
 
 private:
+    std::vector<std::vector<float>> raw_orb_poses;
+    std::vector<std::vector<float>> raw_amcl_poses;
     std::vector<std::vector<float>> orb_poses;
     std::vector<std::vector<float>> amcl_poses;
     Eigen::Matrix4f mtx_opt;
     void fitting();
-    float error(Eigen::Matrix4f*, Eigen::Matrix4f*, Eigen::Matrix4f*);
-    float error(std::vector<float>*, std::vector<float>*);
+    float error(Eigen::Matrix4f&, Eigen::Matrix4f&, Eigen::Matrix4f&);
 
     int itr_num;
     int threshold;
-    int n_data;
+    float time_interval;
+    int tf_compute_interval;
+
+    void load_data(std::vector<std::vector<float>>&, std::vector<std::vector<float>>&);
 };
 }
 
--- src/RANSAC.cc
+++ src/RANSAC.cc
@@ -18,23 +18,70 @@ using namespace Eigen;
 
 namespace ORB_SLAM2
 {
-    RANSAC::RANSAC(std::vector<std::vector<float>> _orb_poses, std::vector<std::vector<float>> _amcl_poses): orb_poses(_orb_poses), amcl_poses(_amcl_poses){
-        n_data = amcl_poses.size();
+    RANSAC::RANSAC(std::vector<std::vector<float>>& _orb_poses, std::vector<std::vector<float>>& _amcl_poses): raw_orb_poses(_orb_poses), raw_amcl_poses(_amcl_poses){
         itr_num = 300;
         threshold = 0.1;
+        time_interval = 10;
+        tf_compute_interval = 10;;
+        load_data(raw_orb_poses, raw_amcl_poses);
         fitting();
+    }
+
+    void RANSAC::load_data(std::vector<std::vector<float>>& raw_orb_poses, std::vector<std::vector<float>>& raw_amcl_poses){
+        std::vector<std::vector<float>> raw_poses_1 = raw_orb_poses;
+        std::vector<std::vector<float>> raw_poses_2 = raw_amcl_poses;
+        std::vector<std::vector<float>> poses_1, poses_2;
+        int idx_1=0;
+
+        raw_poses_1 = (raw_orb_poses.size()>= raw_amcl_poses.size())? raw_orb_poses: raw_amcl_poses;
+        raw_poses_2 = (raw_orb_poses.size()>= raw_amcl_poses.size())? raw_amcl_poses : raw_orb_poses;
+
+        if(idx_1 <raw_poses_1.size()){
+            for(int idx_2=0; idx_2< raw_poses_2.size(); idx_2++){
+                std::vector<float>pose_2 = raw_poses_2.at(idx_2);
+                float time_diff = pose_2.at(0) - (raw_poses_1.at(idx_1)).at(0);
+
+                //find most similar time stamp of each orb_pose
+                while (time_diff>=0){
+                    if (idx_1 < raw_poses_1.size()){
+                        idx_1++;
+                        time_diff = pose_2.at(0) - (raw_poses_1.at(idx_1)).at(0);
+                    }
+                    else
+                        break;
+                }
+                if (idx_1 >=1 && abs(time_diff)>= abs(pose_2.at(0)-(raw_poses_1.at(idx_1-1)).at(0))){
+                    idx_1 --;
+                    time_diff = pose_2.at(0) - (raw_poses_1.at(idx_1)).at(0);
+                }
+
+                //check if the datas are adequate
+                if (abs(time_diff) <= time_interval){
+                    poses_1.push_back(raw_poses_1.at(idx_1));
+                    poses_2.push_back(pose_2);
+                    idx_1++;
+                }
+            }
+        }
+        orb_poses = (raw_orb_poses.size() >= raw_amcl_poses.size()) ? poses_1 : poses_2;
+        amcl_poses = (raw_orb_poses.size() >= raw_amcl_poses.size()) ? poses_2 : poses_1;
+        if (amcl_poses.size() != orb_poses.size())
+            std::cout<< "amcl size: " << amcl_poses.size() << ", orb size: "<< orb_poses.size() << ", Sth is wrong!!" << std::endl;
+        else
+            std::cout<<"datas loaded, size: "<< amcl_poses.size() <<std::endl;
     }
 
     void RANSAC::fitting(){
         int c_max = 0;
         mtx_opt = Matrix4f::Identity();
+        int n_data= amcl_poses.size();
         for (int itr=0; itr<=itr_num; itr++){
             if(n_data ==0){
                 std::cout << "no data" << std::endl;
                 break;
             }
             else{
-                int k = (int)floor(rand()%n_data +1);
+                int k = (int)floor(rand()%n_data);
                 std::vector<float> orb_pose = orb_poses.at(k);
                 std::vector<float> amcl_pose = amcl_poses.at(k);
                 Quaternion<float> q_orb(orb_pose.at(3), orb_pose.at(4), orb_pose.at(5), orb_pose.at(6));
@@ -56,7 +103,7 @@ namespace ORB_SLAM2
                 Eigen::Matrix4f mtx = amcl_trans * orb_trans.inverse();
                 int cnt = 0;
                 for(int i=0; i<=(int)orb_poses.size(); i++){
-                    if(error(&(orb_trans), &(amcl_trans), &mtx) < threshold)
+                    if(error(orb_trans, amcl_trans, mtx) < threshold)
                         cnt ++;
                     if (cnt > c_max){
                         c_max = cnt;
@@ -67,10 +114,10 @@ namespace ORB_SLAM2
         }
     }
 
-    float RANSAC::error(Eigen::Matrix4f* orb_trans, Eigen::Matrix4f* amcl_trans, Eigen::Matrix4f* mtx){
-        Eigen::Matrix4f pred_amcl_trans = *mtx * *orb_trans;
+    float RANSAC::error(Eigen::Matrix4f& orb_trans, Eigen::Matrix4f& amcl_trans, Eigen::Matrix4f& mtx){
+        Eigen::Matrix4f pred_amcl_trans = mtx * orb_trans;
 
-        float err = sqrt(pow((*amcl_trans)(0,3)-pred_amcl_trans(0,3),2)+pow((*amcl_trans)(1,3)-pred_amcl_trans(1,3),2)+pow((*amcl_trans)(2,3)-pred_amcl_trans(2,3),2));
+        float err = sqrt(pow(amcl_trans(0,3)-pred_amcl_trans(0,3),2)+pow(amcl_trans(1,3)-pred_amcl_trans(1,3),2)+pow(amcl_trans(2,3)-pred_amcl_trans(2,3),2));
         return err;
     }
 
